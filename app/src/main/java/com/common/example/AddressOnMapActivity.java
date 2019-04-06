6bffe7520048cc367060f996a6f38d80b71059cc
silviapas <34346967+silviapas@users.noreply.github.com>
2019-04-06 19:22:46 +0200

    Update AddressOnMapActivity.java
    aggiunta di un commento

--- app/src/main/java/com/common/example/AddressOnMapActivity.java
+++ app/src/main/java/com/common/example/AddressOnMapActivity.java
@@ -14,6 +14,8 @@ public class AddressOnMapActivity extends Activity {
     private EditText et_Url;
     private AddressOnMapActivity mContext;
 
+
+/* ciao ciao commit*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
